fa8673534934a9267dc74383a01a5a23c08a6260
Seo Jooyoung <jooyoungseo02@gmail.com>
2024-06-15 08:02:04 +0900

    feat: HomeView 뷰 구현

--- NomaDo/NomaDo/Record.swift
+++ NomaDo/NomaDo/Record.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftData
+ 
+@Model
+class Record {
+    @Attribute(.unique) var id = UUID()
+    var date: Date
+    var togetherPeople: [Int]
+    
+    init(date:Date, togetherPeople: [Int]) {
+        self.date = date
+        self.togetherPeople = togetherPeople
+    }
+}
