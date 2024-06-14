22108e2524a03032ee58a8cd5663e7ca3d9978b7
Seo Jooyoung <jooyoungseo02@gmail.com>
2024-06-15 05:28:16 +0900

    initial commit

---
+++ NomaDo/NomaDo/NomaDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomaDoApp.swift
+//  NomaDo
+//
+//  Created by Seo-Jooyoung on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct NomaDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
